9660f45d0931bdbf59b0bed2e7d7a72c4daeae85
khiev navin <khievnavin@gmail.com>
2021-03-10 13:35:07 +0700

    FirstTest

---
+++ app/src/main/java/com/example/pro1/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.pro1;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout btn;
+    ImageView iv;
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.click);
+        iv=findViewById(R.id.iv);
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                 == PackageManager.PERMISSION_DENIED){
+                  String[] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                  requestPermissions(permission,PERMISSION_CODE);
+                 }
+                 else {
+                     pickImageFromGallery();
+                 }
+                 }
+                 else {
+                    pickImageFromGallery();
+
+                 }
+
+            }
+        });
+    }
+
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if (grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGallery();
+                }
+                else {
+                    Toast.makeText(this,"Permission denies",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE){
+
+           iv.setImageURI(data.getData());
+
+        }
+    }
+}
